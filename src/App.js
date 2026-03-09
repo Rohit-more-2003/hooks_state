eb29f7c6d41db29eac8c809e1d5a5f94037d4e1b
Rohit_More <morer4851@gmail.com>
2026-03-09 11:21:22 +0530

    Integrated form component in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import InputComponent from "./InputComponent";
+import RegisterForm from "./Form";
 
 function App() {
   return (
     <div className="App">
       <InputComponent />
+      <RegisterForm />
     </div>
   );
 }
